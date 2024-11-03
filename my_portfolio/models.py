2aca668dcfffb9710ddc3b11cade996013e816d5
peter <wchegesalome@gmail.com>
2024-11-03 15:42:56 -0500

    project model.

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 # User profile model to store additional information about the website owner (me)
 class UserProfile(models.Model):
+    """The UserProfile model stores user information."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
@@ -13,3 +14,16 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.username
     
+class Project(models.Model):
+    """Project model to store information about each project."""
+    title  = models.CharField(max_length=200)
+    description = models.TextField()
+    technologies = models.CharField(max_length=300) # list technologies as a comma sepatared string
+    live_link = models.URLField(blank=True, null=True)
+    repo_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    # client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True, blank=True, related_name='projects')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
