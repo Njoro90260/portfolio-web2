6767f30e7497ad70df7155f6cc050817f68d1cc9
peter <wchegesalome@gmail.com>
2024-11-03 15:05:43 -0500

    UserProfile model.

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'my_portfolio'
 urlpatterns = [
     # The view page.
     path('', views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
