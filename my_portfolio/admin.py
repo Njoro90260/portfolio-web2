361cd2088af774b61f426a7b22e992c6b061e3e0
peter <wchegesalome@gmail.com>
2024-11-04 12:41:39 -0500

    register models commit 1.

--- my_portfolio/admin.py
+++ my_portfolio/admin.py
@@ -1,3 +1,17 @@
+import os
+import django
 from django.contrib import admin
+from django.apps import apps
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+django.setup()
 # Register your models here.
+
+app = apps.get_app_config('my_portfolio')
+
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
