a7c8de5069c254d1c0f121254bd0d970a37311a7
njoro90620 <wchegesalome@gmail.com>
2024-11-20 13:52:51 +0300

    Django-Debug-Toolbar.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    # Third party apps.
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_portfolio.urls')),
-]
+] + debug_toolbar_urls()
