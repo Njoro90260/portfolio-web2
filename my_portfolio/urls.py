735bc49fff0210a934bfe4d644166e0ad578d2fb
peter <wchegesalome@gmail.com>
2024-12-02 13:11:31 +0000

    user profile final commit.

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from.views import UserProfileDetailView, UserProfileUpdateView
 
 app_name = 'my_portfolio'
 urlpatterns = [
@@ -13,4 +14,7 @@ urlpatterns = [
     path('project/<int:id>', views.project_view, name='project'),
     path('client/<uuid:token>/testimonial/', views.submit_testimonial, name='submit_testimonial'),
     path('thank-you/', views.testimonial_thank_you, name='testimonial_thank_you'),
+    path('profile/<str:username>/', UserProfileDetailView.as_view(), name='profile'),
+    path('profile/<str:username>/edit/', UserProfileUpdateView.as_view(), name='edit_profile'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
