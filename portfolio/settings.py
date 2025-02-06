b319a7e69979c215af12362796407f48e5132d6a
Njoro90260 <wchegesalome@gmail.com>
2025-02-06 18:46:56 +0300

    client static files fixed.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,8 +148,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
