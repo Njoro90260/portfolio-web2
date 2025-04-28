299c6adfc18b0d74aa070415b88a269aecdf6847
Peter Njoroge <wchegesalome@gmail.com>
2025-04-28 22:39:09 +0300

    fixed debug setting and removed load dotenv.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os 
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 # ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',') if host.strip()]
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
